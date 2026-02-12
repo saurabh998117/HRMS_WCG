b32f73ee82b6fb340474c0d55316bd0088a26f51
AnilYadav751 <anilyad292@gmail.com>
2026-02-12 16:33:16 +0530

    Refractor the packages to com.example.admindashboard folder

--- admindashboard/admindashboard/src/main/java/com/example/admindashboard/controller/DashboardController.java
+++ admindashboard/admindashboard/src/main/java/com/example/admindashboard/controller/DashboardController.java
@@ -5,8 +5,6 @@ import com.example.admindashboard.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.security.Principal;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
